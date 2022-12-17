dfc60923dec737d44da915d772411751eba0a887
Cristian Milton Fidel Pascual <al221710403@gmail.com>
2022-12-16 23:32:13 -0600

    Modificación de Jetstream agregacion de columnas

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,13 +15,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->char('username')->unique();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('slug', 100)->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
+            $table->text('meta')->nullable();
             $table->timestamps();
         });
     }
